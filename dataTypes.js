107548396672f2c43fd959a9f5e1bf0579700ebd
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-07 18:16:07 -0500

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,13 @@
+//JavaScript has 7 data types
+//Number including whole numbers and decimals
+//String
+//Boolean
+//Null - absence of value
+//Undefined
+//Symbol
+//Object
+
+console.log("JavaScript")
+console.log(2011)
+console.log("Woohoo! I love to code! #codecademy")
+console.log(20.49)
