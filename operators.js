b6083f8a4cddcef10d5ed75cb94cd0362aa2a634
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-07 19:11:49 -0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,7 @@
+//+ - * / % 
+//These are all called operators
+//concatenation is appending one string to another
+console.log("te" + "st")
+//spells out test
+// . is the dot operator
+console.log("test".length);
